db26365654e60d1a3cb08a6e2067ca7c13e1885f
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-02-06 23:00:40 +0100

    Added installed.json

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '918733553545-hihtke5qa6aqd48s3esmisfabr8di6b5.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'kE17EZhttPcIuxCOSnaYg5QV',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
